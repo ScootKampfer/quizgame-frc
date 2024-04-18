4916bb2d2d1cc81b674a44f617c9731a7b6afcb4
Gabriel Noah Bernier <gabenoah8@gmail.com>
2024-04-17 20:40:55 -0400

    Part 2

--- quiz.py
+++ quiz.py
@@ -1,6 +1,7 @@
-# step 2
-
 from string import ascii_lowercase
+import random
+
+NUM_QUESTIONS_PER_QUIZ = 5
 
 QUESTIONS = {
 
@@ -11,10 +12,29 @@ QUESTIONS = {
 
     "Who won the Chairman's Award at the 2022 Montreal Regional": [
 
-        "2626 - Evolution",
-        "3990 - Tech for Kids",
-        "3544 - Spartiates",
-        "3360 - Hyperion",
+        "2626 - Evolution", "3990 - Tech for Kids", "3544 - Spartiates", "3360 - Hyperion",
+
+    ],
+
+    "Who won the Impact Award at Worlds in 2023": [
+
+        "321 - Robolancers", "2056 - OP Robotics", "5553 - Robo'Lyon", "16 - Bomb Squad"
+    ],
+
+    "Where where the 2 Quebec Regionals in 2020": [
+
+        "Sherbrooke and Montreal", "Montreal and Quebec City", "Montreal and Trois-Rivières", "Quebec City and Gatineau"
+
+    ],
+    
+    "Which Quebec team won Worlds and which year": [
+
+        "296 - 2006", "2016 - 3990", "3996 - 2016", "7605 - 2019"
+    ],
+
+    "Which Quebec team was interviewed by FIRST Update Now in 2024": [
+
+        "3117 - Harfangs", "5528 - Ultime AGT", "9624 - Dynamiques", "9406 - TechJunior"
     ],
 
     "Who won the Impact Award at Worlds in 2023": [
@@ -23,21 +43,28 @@ QUESTIONS = {
     ],
 }
 
-for question, alternatives in QUESTIONS.items():
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
 
+num_correct = 0
+for num, (question, alternatives) in enumerate(questions, start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-
-    for label, alternative in enumerate(sorted_alternatives):
-
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    )
+    for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
 
-    answer_label = int(input(f"{question}? "))
-    answer = sorted_alternatives[answer_label]
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
 
+    answer = labeled_alternatives[answer_label]
     if answer == correct_answer:
-
-        print("Correct!")
+        num_correct += 1
+        print("⭐ Correct! ⭐")
     else:
-
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {num_correct} correct out of {num} questions")
